f66a889d0d2bf0235219ad9e9d08340c0b3c3033
Terrance Chang <tchang46343@gmail.com>
2019-12-15 18:45:49 -0800

    Pushed local backend of mind-game-api to heroku

--- src/app.js
+++ src/app.js
@@ -18,9 +18,18 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
-  //   res.send("Hello, basic boilerplate!");
 });
 
 app.use(function errorHandler(error, req, res, next) {
