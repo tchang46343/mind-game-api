7baafcb2aa1cf88c5e0a6963b46a42169e20836f
Terrance Chang <tchang46343@gmail.com>
2020-03-12 14:59:27 -0700

    Modify seed files for heroku

--- seeds/seed.gameslides.sql
+++ seeds/seed.gameslides.sql
@@ -3,36 +3,36 @@ values
 
   ('Alleviation: The contemplation of your own thoughts and desires and conduct.',
   'Jus do it! -Phil Night',
-  'https://www.dropbox.com/home/Relaxing%20Images?preview=Snow.jpg'),
+  'https://c.pxhere.com/photos/0a/38/lake_snow_frozen_winter_cold_ice_white_nature-1280424.jpg!d'),
   
   ('Chill:Relax or to hang out.',
   'Relax, Recharge and Reflect. Sometimes it’s OK to do nothing. ―Izey Victoria Odiase',
-  'https://www.dropbox.com/home/Relaxing%20Images?preview=English-Cream-Retriever.jpg'),
+  'https://s3.amazonaws.com/assets.prod.vetstreet.com/12/b0/35b04a9b4a558785b2eff02cc8b5/golden-retriever-puppy-ap-07vqqk-335.jpg'),
   
   ('Tranquil:Not excited or emotionally agitated.',
   'Innovation distinguishes between a leader and a follower -Steve Jobs',
-  'https://www.dropbox.com/home/Relaxing%20Images?preview=RelaxingCat.png'),
+  'https://static.nationalgeographic.co.uk/files/004-h_21_00168294-best-adventure-islands.jpg'),
   
   ('Stillness:Absence of motion or disturbance.',
   'Tension is who you think you should be. Relaxation is who you are. -Chinese Proverb',
-  'https://www.dropbox.com/home/Relaxing%20Images?preview=Screen+waterfall.jpg'),
+  'https://files.tripstodiscover.com/files/2018/03/moraine-lake-canada-3.jpg'),
   
   ('Soothe:To ease the anger of agitation of.',
   'You get in life what you have the courage to ask for. -Oprah Winfrey',
-  'https://www.dropbox.com/home/Relaxing%20Images?preview=coding.jpg'),
+  'https://i.pinimg.com/originals/c4/94/f3/c494f39cb3e86de7072796434e39ba23.jpg'),
   
   ('Beach: A calm relaxing conjunction of where the land meets the ocean in harmony.',
   'Success isn’t always about ‘Greatness’, it’s about consistency. Consistent, hard work gains success. Greatness will come -Dwayne Johnson',
-  'https://www.dropbox.com/home/Relaxing%20Images?preview=Bigasslake.jpg'),
+  'https://images.unsplash.com/photo-1528716321680-815a8cdb8cbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
   
   ('Mellow: Pleasantly smooth or soft; free from harshness.',
   'Never cry. Never complain. Just work. -Floyd Mayweather',
-  'https://www.dropbox.com/home/Relaxing%20Images?preview=sucess.jpg'),
+  'https://images-na.ssl-images-amazon.com/images/I/91jdwk5T5yL._AC_SL1500_.jpg'),
  
   ('Compose:To deal with or act on so as to reduce to a minimum.',
   'Live as if you were to die tomorrow. Learn as if you were to live forever. -Mahatma Gandhi',
-  'https://www.dropbox.com/home/Relaxing%20Images?preview=power.jpg'),
+  'https://images.newindianexpress.com/uploads/user/imagelibrary/2019/10/6/w900X450/GANDHIAN12.jpg'),
  
   ('Reposeful: A kind to induce ease and relaxation.',
   'Many of lifes failures are experienced by people who did not realize how close they were to success when they gave up. -Thomas Edison',
-  'https://www.dropbox.com/home/Relaxing%20Images?preview=wakeup.jpeg');
+  'https://www.africanslive.com/wp-content/uploads/2019/10/30-positive-mental-health-quotes-to-remind-you-of-its-importance.jpg');
