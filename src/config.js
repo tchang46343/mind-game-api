2059dc6bfc723d36a396245cab6291fb330c68c6
Terrance Chang <tchang46343@gmail.com>
2020-03-18 17:17:30 -0700

    URL

--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
     process.env.DATABASE_URL ||
-    "postgresql://tchang@localhost/mindgame_gameslides",
+    "postgres://enybafyxnmjytr:f1b7c0332259b0ed0f0e30c706393cb0f3fc67c5f4842cdc97b37586ffba924c@ec2-35-172-85-250.compute-1.amazonaws.com:5432/dfh9m45h8hun9b",
+  //"postgresql://tchang@localhost/mindgame_gameslides",
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: "3 days"
 };
